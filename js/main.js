965602220d32bc5f5d65734884583acb397d993a
Bim Williams <bim@MrBim-MBP.local>
2017-08-17 15:48:55 +0100

    spiders have completed training

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+var speed1 = (Math.floor(Math.random() * (2000 - 200)) + 200)/100; //The maximum is exclusive and the minimum is inclusive
+var speed2 = (Math.floor(Math.random() * (2000 - 200)) + 200)/100; //The maximum is exclusive and the minimum is inclusive
+var speed3 = (Math.floor(Math.random() * (2000 - 200)) + 200)/100; //The maximum is exclusive and the minimum is inclusive
+var speed4 = (Math.floor(Math.random() * (2000 - 200)) + 200)/100;  //The maximum is exclusive and the minimum is inclusive
+
+
+TweenMax.to(".spider1", speed1, {left:1200});
+TweenMax.to(".spider2", speed2, {left:1200});
+TweenMax.to(".spider3", speed3, {left:1200});
+TweenMax.to(".spider4", speed4, {left:1200});
+
+var position1 = $('.spider1').css('left');
+var position1 = $('.spider1').css('left');
+var position1 = $('.spider1').css('left');
+var position1 = $('.spider1').css('left');
+ 
